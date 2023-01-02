d6b8064f85eb12ab7577553c222c27243c113bd9
zhengsw <zhengsw@centos7.zhengsw.309>
2023-01-02 20:54:46 +0800

    郑思文: 使能 lsp-mode 的补全功能, 禁止 lsp-mode 自动编辑代码; 简化 emacs 退出时的确认提示为 y-or-n; 1) package: 禁用 cmake-ide 之后 company-mode 依赖 clang 进行补全，但 company-clang-args 没有能被配置;    - lsp-mode: 放开之前禁用的 lsp-completion 补全功能，lsp-completion-enable;    - lsp-mode: 将 lsp-completion-enable-additional-text-edit 设置为 nil，禁用 lsp 补全时自动编辑代码,      例如自动添加 include 头文件; 2) customize: 将 confirm-kill-emacs 配置为 y-or-n-p, 简化输入，'yes-or-no-p' 比较麻烦;

--- emacs/.emacs.d/config/prog-lang.el
+++ emacs/.emacs.d/config/prog-lang.el
@@ -19,7 +19,8 @@
 (use-package lsp-ui)
 (use-package lsp-mode
   :hook ((c-mode c++-mode python-mode) . lsp)
-  :custom (lsp-completion-enable (not (derived-mode-p 'c-mode 'c++-mode)) "use company-mode to do completion")
+  ;; :custom (lsp-completion-enable (not (derived-mode-p 'c-mode 'c++-mode)) "use company-mode do completion other than lsp-completion.")
+  :custom (lsp-completion-enable-additional-text-edit nil "disable lsp-mode edit file, eg. insert include header file.")
   :custom (lsp-enable-on-type-formatting nil)
   :bind-keymap ("C-c u" . lsp-command-map)
   )
